dbe7c381dd3fffd91a07032ba7ea9421e02a2865
Rafii-Ayoub <72503025+Rafii-Ayoub@users.noreply.github.com>
2022-03-22 22:28:40 +0100

    First Commit
    Signed-off-by: Rafii-Ayoub <72503025+Rafii-Ayoub@users.noreply.github.com>

---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,50 @@
+import "./sidebar.css";
+import {
+  RssFeed,
+  Chat,
+  PlayCircleFilledOutlined,
+  Group,
+  Bookmark,
+  HelpOutline,
+  WorkOutline,
+  Event,
+  School,
+} from "@material-ui/icons";
+import { Users } from "../../dummyData";
+import CloseFriend from "../closeFriend/CloseFriend";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          
+          <li className="sidebarListItem">
+            <Chat className="sidebarIcon" />
+            <span className="sidebarListItemText">Chats</span>
+          </li>
+          
+          <li className="sidebarListItem">
+            <Group className="sidebarIcon" />
+            <span className="sidebarListItemText">Groups</span>
+          </li>
+          <li className="sidebarListItem">
+            <HelpOutline className="sidebarIcon" />
+            <span className="sidebarListItemText">Questions</span>
+          </li>
+          <li className="sidebarListItem">
+            <Event className="sidebarIcon" />
+            <span className="sidebarListItemText">Events</span>
+          </li>
+        </ul>
+        <button className="sidebarButton">Show More</button>
+        <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+          {Users.map((u) => (
+            <CloseFriend key={u.id} user={u} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ client/src/dummyData.js
@@ -0,0 +1,19 @@
+export const Users = [
+  {
+    id:1,
+    profilePicture: "person/1.PNG",
+    username: "Rafii Ayoub",
+  },
+  {
+    id:2,
+    profilePicture: "person/2.PNG",
+    username: "Bekkari Ahmed",
+  },
+  {
+    id:3,
+    profilePicture: "person/3.PNG",
+    username: "Marc Phillipe Huget",
+  }
+
+  
+];
